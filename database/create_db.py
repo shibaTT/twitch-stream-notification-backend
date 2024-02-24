6cb7fad4093b6cd5c6f995fd9e2139a5143ac9d6
ShibaTTTTTTTTT <39818834+shibaTT@users.noreply.github.com>
2024-02-24 17:44:45 +0900

    update: 配信者情報に配信者名のカラムを追加

--- database/create_db.py
+++ database/create_db.py
@@ -20,15 +20,18 @@ cur.execute(
 # streamersテーブルの作成
 # streamer_id（通知する配信者のbroadcasterID）、
 # streamer_login_id（配信者のloginID（URLに表示されてるやつ））、
+# streamer_name（配信者の表示名）
 # channel_id（通知が送信されるチャンネルID）、
 # is_active（通知がアクティブかどうか）
+# 各日時
 cur.execute(
-    "CREATE TABLE IF NOT EXISTS streamers (streamer_id VARCHAR(32) PRIMARY KEY, streamer_login_id VARCHAR(32), channel_id VARCHAR(32), is_active BOOLEAN, created_at datetime, updated_at datetime, deleted_at datetime)"
+    "CREATE TABLE IF NOT EXISTS streamers (streamer_id VARCHAR(32) PRIMARY KEY, streamer_login_id VARCHAR(32), streamer_name VARCHAR(128), channel_id VARCHAR(32), is_active BOOLEAN, created_at datetime, updated_at datetime, deleted_at datetime)"
 )
 
 # channelsテーブルの作成
 # channel_id（チャンネルID）、channel_name（チャンネル名）、
 # guild_id（サーバーのID）、webhook_url（WebhookURL）
+# 各日時
 cur.execute(
     "CREATE TABLE IF NOT EXISTS channels (channel_id VARCHAR(32) PRIMARY KEY, channel_name VARCHAR(128), guild_id VARCHAR(32), webhook_url VARCHAR(256), created_at datetime, updated_at datetime, deleted_at datetime)"
 )
